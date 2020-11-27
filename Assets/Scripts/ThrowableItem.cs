e83e048e9ba545bdbdec64664e505563833369ce
outerspace <hello@outerspace.pl>
2020-11-27 23:45:40 +0100

    initial throw weapon mechanics, collision layers, object tags

--- Assets/Scripts/ThrowableItem.cs
+++ Assets/Scripts/ThrowableItem.cs
@@ -39,7 +39,7 @@ public class ThrowableItem : MonoBehaviour
     {
         if (col.gameObject.tag == "DieCollider")
         {
-            Explode();
+            Destroy(gameObject);
         }
     }
 
