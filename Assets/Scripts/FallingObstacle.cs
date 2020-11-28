7f07c77ff0ab009e3fd80013e6d358398e505ded
outerspace <hello@outerspace.pl>
2020-11-28 01:10:06 +0100

    falling bricks mechanics init

---
+++ Assets/Scripts/FallingObstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObstacle : MonoBehaviour
+{
+    public ParticleSystem particleSys = default;
+    [SerializeField] GameObject exploPrefab = default;
+    [SerializeField] GameObject spawnedExploParent = default;
+
+    GameObject exploHandler = null;
+    List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>(5);
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = particleSys.GetCollisionEvents(other, collisionEvents);
+        if (numCollisionEvents > 0)
+        {
+            SpawnExplosion(collisionEvents[0].intersection);
+        }
+    }
+
+    private void SpawnExplosion(Vector3 exploPos)
+    {
+        exploHandler = Instantiate(exploPrefab, exploPos, Quaternion.identity);
+        exploHandler.transform.parent = spawnedExploParent.transform;
+    }
+}
