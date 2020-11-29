e587a8920a5bd1206215888fad96d4a0334d4934
outerspace <hello@outerspace.pl>
2020-11-29 02:06:39 +0100

    start menu, music, build settings

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer instance;
+    private void Awake()
+    {
+        if (instance == null) { instance = this; DontDestroyOnLoad(gameObject); }
+        else { Destroy(gameObject); }
+    }
+}
